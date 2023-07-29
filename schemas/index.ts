4b4e973ea439b2e38f27f742edd5d95c1eebf03c
Harsimranji-singh <hsdosanjh81468@gmail.com>
2023-07-28 19:27:48 -0700

    first commit

---
+++ schemas/index.ts
@@ -0,0 +1,20 @@
+import testimonilas from './testimonials'
+import about from './about'
+import brand from './brands'
+import contact from './contact'
+import experiance from './experience'
+import skills from './skills'
+import workexperience from './workexperience'
+import work from './work'
+import portfolio from './portfolio'
+export const schemaTypes = [
+  testimonilas,
+  about,
+  brand,
+  contact,
+  experiance,
+  skills,
+  workexperience,
+  work,
+  portfolio,
+]
---
+++ schemas/portfolio.js
@@ -0,0 +1,17 @@
+export default {
+  title: 'Manuscript',
+  name: 'manuscript',
+  type: 'document',
+  fields: [
+    {
+      name: 'description',
+      type: 'string',
+      title: 'Description',
+    },
+    {
+      name: 'portfolio',
+      type: 'file',
+      title: 'Portfolio',
+    },
+  ],
+}
